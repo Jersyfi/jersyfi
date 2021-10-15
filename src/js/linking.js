534159348f74886469a55bb99155768b21e62641
Jérôme Bastian Winkel <64214261+Jersyfi@users.noreply.github.com>
2021-10-15 10:59:24 +0200

    Update linking.js

--- src/js/linking.js
+++ src/js/linking.js
@@ -1,14 +1,14 @@
 var text = {
     en: {
-        header: '%cMade with Laravel & %c❤ %cby Jersyfi',
+        header: '%cMade with %c❤ %cby Jersyfi',
         body: '%cFor more information about me and my work visit https://jersyfi.de or https://github.com/Jersyfi'
     },
     de: {
-        header: '%cErstellt mit Laravel & %c❤ %cvon Jersyfi',
+        header: '%cErstellt mit %c❤ %cvon Jersyfi',
         body: '%cBesuche https://jersyfi.de oder https://github.com/Jersyfi um mehr Informationen über mich und meine Arbeit zu bekommen'
     },
     no: {
-        header: '%cCreated with Laravel & %c❤ %cav Jersyfi',
+        header: '%cCreated with %c❤ %cav Jersyfi',
         body: '%cBesøk https://jersyfi.de eller https://github.com/Jersyfi for å få mer informasjon om meg og arbedet mitt'
     }
 }
