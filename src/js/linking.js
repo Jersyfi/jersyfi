67966b6b6a1efc18abd12e047d4f2bebc7c12be4
Jérôme Bastian Winkel <64214261+Jersyfi@users.noreply.github.com>
2021-10-15 10:57:28 +0200

    Create linking.js

---
+++ src/js/linking.js
@@ -0,0 +1,31 @@
+var text = {
+    en: {
+        header: '%cMade with Laravel & %c❤ %cby Jersyfi',
+        body: '%cFor more information about me and my work visit https://jersyfi.de or https://github.com/Jersyfi'
+    },
+    de: {
+        header: '%cErstellt mit Laravel & %c❤ %cvon Jersyfi',
+        body: '%cBesuche https://jersyfi.de oder https://github.com/Jersyfi um mehr Informationen über mich und meine Arbeit zu bekommen'
+    },
+    no: {
+        header: '%cCreated with Laravel & %c❤ %cav Jersyfi',
+        body: '%cBesøk https://jersyfi.de eller https://github.com/Jersyfi for å få mer informasjon om meg og arbedet mitt'
+    }
+}
+
+switch (navigator.language) {
+    case 'de':
+        loadText = text.de
+        break;
+
+    case 'no':
+        loadText = text.no
+        break;
+
+    default:
+        loadText = text.en
+        break;
+}
+
+console.log(loadText.header, 'color: blue; font-size: 28px;', 'color: red; font-size: 28px;', 'color: blue; font-size: 28px;')
+console.log(loadText.body, 'font-size: 16px;')
